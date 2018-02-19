3fb99e266c89c018a1e329801b0911433b098cc8
Thomas Haslwanter <thomas.haslwanter@alumni.ethz.ch>
2018-02-19 21:15:16 +0100

    NGIMUs added

---
+++ skinematics/tests/test_sensor_xio_ngimu.py
@@ -0,0 +1,37 @@
+'''
+Test import from import data saved with NGIMU sensors from x-io, through subclassing "IMU_Base"
+'''
+
+'''
+Author: Thomas Haslwanter
+Date: Feb-2018
+'''
+
+import sys
+import os
+myPath = os.path.dirname(os.path.abspath(__file__))
+sys.path.insert(0, os.path.join(myPath, '..'))
+
+import unittest
+import imus
+from time import sleep
+from sensors.xio_ngimu import NGIMU
+
+class TestSequenceFunctions(unittest.TestCase):
+    
+    def test_import_xio(self):
+        # Get data, with a specified input from an XIO system
+        in_file = os.path.join(myPath, 'data', 'data_ngimu')
+        sensor = NGIMU(in_file=in_file, q_type=None)
+        
+        rate = sensor.rate
+        acc = sensor.acc
+        omega = sensor.omega
+        
+        self.assertAlmostEqual((rate - 50), 0)
+        self.assertAlmostEqual( (omega[0,2] + 0.0020045), 0)
+        
+if __name__ == '__main__':
+    unittest.main()
+    print('Thanks for using programs from Thomas!')
+    sleep(0.2)
